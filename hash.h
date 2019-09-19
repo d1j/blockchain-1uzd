c064f5342dca93739c8ab48b76042eb45d2c1db7
Deividas Januska <dj9909@gmail.com>
2019-09-19 17:47:44 +0300

    v0.1 final

--- hash.h
+++ hash.h
@@ -3,14 +3,6 @@
 
 #include "includes.h"
 
-uint32_t _4byte_string_to_uint32_t(string);
-
 string hash(string);
-
-uint32_t F(uint32_t, uint32_t, uint32_t);
-
-uint32_t G(uint32_t X, uint32_t Y, uint32_t Z);
-
-uint32_t H(uint32_t X, uint32_t Y, uint32_t Z);
 
 #endif
